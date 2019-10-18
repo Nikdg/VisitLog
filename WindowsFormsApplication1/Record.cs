58f756cc51f0ba473fd3e89d9748d1d1a0a1e699
101 <101@ctt.perm.ru>
2019-10-18 21:02:17 +0500

    rev2

---
+++ WindowsFormsApplication1/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Record : IRecord
+    {
+        public void RecordVisit(string name, string surname, int age, string employeePosition, string subject)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Student : Person
+    {
+        private string faculty;
+        private string specialty;
+        private string group;
+        public Student(string name, string surname, int age, string faculty, string specialty, string group)
+            :base(name,surname,age)
+        {
+            this.faculty = faculty;
+            this.specialty = specialty;
+        }
+        public string Faculty
+        {
+            get
+            {
+                return faculty;
+            }
+            set
+            {
+                faculty = value;
+            }
+        }
+        public string Specialty
+        {
+            get
+            {
+                return specialty;
+            }
+            set
+            {
+                specialty = value;
+            }
+        }
+        public string Group
+        {
+            get
+            {
+                return group;
+            }
+            set
+            {
+                group = value;
+            }
+        }
+    }
+}
